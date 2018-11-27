d32dea5e0e8be87773825dfba8f9ab3679a81694
m.izobava <m.izobava@gmail.com>
2018-11-28 00:02:43 +0300

    minor changes

--- OneWriteMultRead/OneWriteMultRead.cpp
+++ OneWriteMultRead/OneWriteMultRead.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <mutex>
 #include <cassert>
-#include <time.h>
 #include <sstream>
 #include <vector>
 #include <algorithm>
@@ -21,15 +20,21 @@ struct one_shot {
 private:
 	std::condition_variable cv_;
 	std::mutex mx_;
+	bool f = false;
 
 public:
 	void wait() {
-		std::unique_lock<std::mutex> lock(mx_);
-		cv_.wait(lock);
+			std::unique_lock<std::mutex> lock(mx_);
+			cv_.wait(lock, [&]() {return f; });
 	}
 
 	void signal() {
-		cv_.notify_one();
+		{
+			std::lock_guard<std::mutex> lock(mx_);
+			f = true;
+		}
+	
+		cv_.notify_all();
 	}
 };
 
@@ -41,28 +46,31 @@ void write(int id) {
 			std::unique_lock<std::mutex> lock(mx_);
 			writer_is_active_ = true;
 			writer_.wait(lock, []() {return !number_of_readers; });
-			int timer_ = 2;
-			std::cout << "Thread #" << id << " gonna write for " << timer_ << " seconds\n";
-			std::this_thread::sleep_for(std::chrono::seconds(timer_));
+			std::cout << "Thread #" << id << " gonna write for " << 3 << " seconds\n";
+			std::this_thread::sleep_for(std::chrono::seconds(3));
 			writer_is_active_ = false;
 		}
+	readers_.notify_all();
 
 }
 
 
 void read(int id) {
 	one_shot_signal.wait();
-		while (writer_is_active_) {}
+	{
+		std::unique_lock<std::mutex> lock(mx_);
+		readers_.wait(lock, []() { return !writer_is_active_; });
 		number_of_readers++;
-		std::stringstream stream;
-		stream << "Thread #" << id << " gonna read for 1 seconds\n";
-		std::cout << stream.str();
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-		number_of_readers--;
-		writer_.notify_all();
+	}
+	std::stringstream stream;
+	for (int i = 0; i < id; i++)
+	stream << "    ";
+	stream << "read " << id << "\n";
+	std::cout << stream.str();
+	number_of_readers--;
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	writer_.notify_all();
 }
-
-
 
 const int n_readers = 5;
 const int n_writers = 6;
@@ -70,19 +78,30 @@ const int n_writers = 6;
 
 int main()
 {
-	srand(time(NULL));
-	
-	std::vector<std::thread> readers, writers;
-	readers.reserve(n_readers);
-	writers.reserve(n_writers);
-	for (int i = 0; i < n_writers; i++)
-		writers.emplace_back(write, i + 1);
-	for (int i = 0; i < n_readers; i++)
-		readers.emplace_back(read, i + 1);
+
+	std::vector<std::thread> threads;
+	threads.reserve(20);
+
+	threads.emplace_back(read, 1);
+
+	threads.emplace_back(write, 1);
+
+	threads.emplace_back(read, 2);
+
+	threads.emplace_back(write, 2);
+
+	threads.emplace_back(write, 3);
+
+	threads.emplace_back(read, 3);
+
+	threads.emplace_back(write, 4);
+
+	threads.emplace_back(write, 5);
+
+	threads.emplace_back(read, 4);
+
 	one_shot_signal.signal();
-	
-	std::for_each(readers.begin(), readers.end(), [](auto& x) {x.join(); });
-	std::for_each(writers.begin(), writers.end(), [](auto& x) {x.join(); });
 
+	std::for_each(threads.begin(), threads.end(), [](auto& x) {x.join(); });
+	getchar();
 }
-
