51603ed9a34708e3560a37273fdefe1b8acd0f8f
m.izobava <maryia.izobava@solarwinds.com>
2018-12-03 09:16:01 +0300

    Add RuleOfFive idiom implementation

--- OneWriteMultRead/unique_read_lock.h
+++ OneWriteMultRead/unique_read_lock.h
@@ -15,6 +15,20 @@ public:
 
 	unique_read_lock(const unique_read_lock&) = delete;
 
+	unique_read_lock(unique_read_lock && obj) : mx_(obj.mx_) {
+		obj.mx_ = nullptr;
+	}
+
+	unique_read_lock& operator=(unique_read_lock const & obj) = delete;
+
+	unique_read_lock& operator=(unique_read_lock&& obj) {
+		mx_ = obj.mx_;
+		
+		
+		obj.mx_ = nullptr;
+		return *this;
+	}
+
 	~unique_read_lock() {
 		mx_->stop_read();
 	}
--- OneWriteMultRead/unique_write_lock.h
+++ OneWriteMultRead/unique_write_lock.h
@@ -15,6 +15,18 @@ public:
 
 	unique_write_lock(const unique_write_lock&) = delete;
 
+	unique_write_lock(unique_write_lock && obj) : mx_(obj.mx_) {
+		obj.mx_ = nullptr;
+	}
+
+	unique_write_lock& operator=(unique_write_lock const & obj) = delete;
+
+	unique_write_lock& operator=(unique_write_lock&& obj) {
+		mx_ = obj.mx_;
+		obj.mx_ = nullptr;
+		return *this;
+	}
+
 	~unique_write_lock() {
 		mx_->stop_write();
 	}
